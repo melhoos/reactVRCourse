edd8f97ce3df4ca487acc54592a7162054c7cfd9
melhoos <mmelhoos@gmail.com>
2018-01-28 16:20:36 +0100

    added solutions folder

--- Basics/vr/client.js
+++ Basics/vr/client.js
@@ -8,14 +8,15 @@ import {VRInstance} from 'react-vr-web';
 function init(bundle, parent, options) {
   const vr = new VRInstance(bundle, 'Basics', parent, {
     // Add custom options here
+    
     ...options,
   });
   vr.render = function() {
     // Any custom behavior you want to perform on each frame goes here
+    
   };
   // Begin the animation loop
   vr.start();
   return vr;
 }
-
 window.ReactVR = {init};
