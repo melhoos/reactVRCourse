6ed46491e70f8e094530f7758a47818bad810d49
melhoos <mmelhoos@gmail.com>
2018-02-20 17:08:32 +0100

    moved deathStar to its own component

---
+++ reactVR_course/vr/components/deathStar.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  asset,
+  Model,
+  Animated
+} from 'react-vr';
+import { Easing } from 'react-native';
+
+export default class DeathStar extends React.Component {
+  constructor() {
+    super();
+    this.state = { 
+      spin: new Animated.Value(0)
+    };
+  }
+
+  componentDidMount() {
+    this.spinAnimation();
+  }
+
+  spinAnimation() {
+    this.state.spin.setValue(0);
+    Animated.timing(
+      this.state.spin,
+      {
+       toValue: 1,
+       duration: 5000,
+       easing: Easing.linear
+      }
+    ).start( () => this.spinAnimation());
+  }
+
+  render() {
+    const spin = this.state.spin.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    });
+    const AnimatedModel = Animated.createAnimatedComponent(Model);
+    return (
+        <AnimatedModel
+          wireframe={false}
+          source={{
+            obj: asset('death-star.obj'),
+          }}
+          style={{
+            transform: [
+              {translate: [2,0,-4]},
+              {rotateY: spin}
+            ]
+          }}
+          texture={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/827672/death-star.png"}
+        />
+    );
+  }
+};
