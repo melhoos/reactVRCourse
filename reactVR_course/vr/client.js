ad87122f6e256c9381a331e52b5783e585aac34e
Sindre Magnussen <sindre.magnussen@itera.no>
2018-02-26 12:43:59 +0100

    feat: prepare client.vr

--- reactVR_course/vr/client.js
+++ reactVR_course/vr/client.js
@@ -14,11 +14,10 @@ function init(bundle, parent, options) {
   // Add polyfill to allow for VR on iOS devices
   const polyfill = new WebVRPolyFill();
 
-  // Create a div where the overlay will be displayed in the DOM.
-  const domOverlayContainer = document.createElement('div');
-  domOverlayContainer.id = 'dom-overlay';
-  // Create an instance of the module, to be registered below.
-  const domOverlayModule = new DomOverlayModule(domOverlayContainer);
+  // TODO: Comment in to enable Native Module 
+  // const domOverlayContainer = document.createElement('div');
+  // domOverlayContainer.id = 'dom-overlay';
+  // const domOverlayModule = new DomOverlayModule(domOverlayContainer);
 
   const vr = new VRInstance(bundle, 'IteraGame', parent, {
     // Add custom options here
@@ -31,12 +30,12 @@ function init(bundle, parent, options) {
     cursorVisibility: 'visible', */
     enableHotReload: true,
     ...options,
-    // Register dom overlay module upon initialization.
-    nativeModules: [domOverlayModule],
+    // TODO: Comment in to enable Native Module
+    //nativeModules: [domOverlayModule],
   });
 
-  // Inject DOM overlay container to the player so that it is rendered properly.
-  vr.player._wrapper.appendChild(domOverlayContainer);
+  // TODO: Comment in to enable Native Module
+  //vr.player._wrapper.appendChild(domOverlayContainer);
 
   vr.render = function() {
     // Any custom behavior you want to perform on each frame goes here
