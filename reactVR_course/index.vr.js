3d4181748f92ab79a3d64ac82f0900937ab05470
melhoos <mmelhoos@gmail.com>
2018-02-26 17:59:15 +0100

    Removed Text from index.vr.js

--- reactVR_course/index.vr.js
+++ reactVR_course/index.vr.js
@@ -8,20 +8,6 @@ export default class IteraGame extends React.Component {
     return (
       <View>
         <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -6]}],
-          }}>
-          Velkommen til ReactVR kurs! 
-        </Text>
       </View>
     );
   }
