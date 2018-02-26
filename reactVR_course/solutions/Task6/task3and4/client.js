981966281c0ac769ed37bd28735f87493b0d0fd9
Sindre Magnussen <sindre.magnussen@itera.no>
2018-02-26 12:44:26 +0100

    feat: Add solution to task 6

---
+++ reactVR_course/solutions/Task6/task3and4/client.js
@@ -0,0 +1,47 @@
+// Auto-generated content.
+// This file contains the boilerplate to set up your React app.
+// If you want to modify your application, start in "index.vr.js"
+
+// Auto-generated content.
+// Import process.js file which contains definitions required by react-dom
+// to run on the client.
+import '../process';
+import {VRInstance} from 'react-vr-web';
+import DomOverlayModule from '../DomOverlayModule';
+import WebVRPolyFill from 'webvr-polyfill';
+
+function init(bundle, parent, options) {
+  // Add polyfill to allow for VR on iOS devices
+  const polyfill = new WebVRPolyFill();
+
+  const domOverlayContainer = document.createElement('div');
+  domOverlayContainer.id = 'dom-overlay';
+  const domOverlayModule = new DomOverlayModule(domOverlayContainer);  
+
+  const vr = new VRInstance(bundle, 'IteraGame', parent, {
+    // Add custom options here
+    /* raycasters: [{
+      getType: () => "simple",
+      getRayOrigin: () => [0, 0, 0],
+      getRayDirection: () => [0, 0, -1],
+      drawsCursor: () => true
+    }],
+    cursorVisibility: 'visible', */
+    enableHotReload: true,
+    ...options,
+    // Register dom overlay module upon initialization.
+    nativeModules: [domOverlayModule],
+  });
+
+  // Inject DOM overlay container to the player so that it is rendered properly.
+  vr.player._wrapper.appendChild(domOverlayContainer);
+
+  vr.render = function() {
+    // Any custom behavior you want to perform on each frame goes here
+  };
+  // Begin the animation loop
+  vr.start();
+  return vr;
+}
+
+window.ReactVR = {init};
