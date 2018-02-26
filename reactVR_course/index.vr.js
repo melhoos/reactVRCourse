3bb7f95f8b112beaa0a40c1bd9c5bbb279d8e910
melhoos <mmelhoos@gmail.com>
2018-02-26 17:49:19 +0100

    removed yarn.lock file

--- reactVR_course/index.vr.js
+++ reactVR_course/index.vr.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {AppRegistry, asset, Pano, View} from 'react-vr';
+import {AppRegistry, asset, Pano, View, Text} from 'react-vr';
 
 //import IntroText from './components/introduction/IntroText';
 
@@ -8,6 +8,20 @@ export default class IteraGame extends React.Component {
     return (
       <View>
         <Pano source={asset('chess-world.jpg')}/>
+        <Text
+          style={{
+            backgroundColor: '#777879',
+            fontSize: 0.8,
+            fontWeight: '400',
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.2,
+            paddingRight: 0.2,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            transform: [{translate: [0, 0, -6]}],
+          }}>
+          Velkommen til ReactVR kurs! 
+        </Text>
       </View>
     );
   }
